561e064592a22bdc0e8df5ed4ad149e03c5e7611
Burcu <baran.burcu@gmail.com>
2014-04-12 21:39:49 -0700

    change in file

--- getlocations.py
+++ getlocations.py
@@ -14,25 +14,25 @@ def getuserid(x):
     return str(d['id'])
 
 
-#get the locations that the username have been
+#get the locations that the username has been
 
 def getlocation(x):
     id = getuserid(x)  
     req=requests.get("https://api.instagram.com/v1/users/"+id+"/media/recent/?access_token=232690335.f8c47bd.caf347ce037a42c29805a4f389aaf788")
     js = req.json()
     d = dict(js)
-    lis=d['data']
+    alist=d['data']
     
 
      
     locations=[]
 
-    for i in range(len(lis)):
-        di=lis[i]
-        diloc=di['location']
-        if type(diloc) is dict:
-            if 'name' in diloc.keys():
-                locations.append(diloc['name'])
+    for i in range(len(alist)):
+        adic=alist[i]
+        dicloc=adic['location']
+        if type(dicloc) is dict:
+            if 'name' in dicloc.keys():
+                locations.append(dicloc['name'])
            
     return locations
      
